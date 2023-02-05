0a6d02ea9f85b0a2b1c80b2ae57bc85bad8a14ef
Irieo <iegor.riepin@gmail.com>
2023-02-05 18:46:45 +0100

    patch merge_plots to treat pdf only

--- scripts/merge_plots.py
+++ scripts/merge_plots.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # Detect running outside of snakemake and mock snakemake for testing
     if 'snakemake' not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake('merge_plots', year='2025', zone='IE', palette='p1', policy="cfe100") 
+        snakemake = mock_snakemake('merge_plots', year='2025', zone='IEDK', palette='p1', policy="cfe100") 
 
 # Call the PdfFileMerger
 mergedObject = PdfMerger()
@@ -15,7 +15,8 @@ path = snakemake.output[0][:-11]
 
 # Loop through existing plots in output repository and append them
 for file in os.listdir(path):
-    mergedObject.append(PdfReader(path+f'{file}', 'rb'))
+    if file.endswith('.pdf'):
+        mergedObject.append(PdfReader(path+f'{file}', 'rb'))
 
 # Write merged pdf
 mergedObject.write(path+"/"+"SUMMARY.pdf")
